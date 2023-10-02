6894c6475da32555c9060765985a04a5397f439e
tramy74 <96005295+tramy74@users.noreply.github.com>
2023-10-02 22:07:25 +0700

    first commit

---
+++ app/components/Task.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { ITask } from "@/types/tasks";
+import { FormEventHandler, useState } from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
+import { GrFormView } from "react-icons/gr";
+import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { deleteTodo, editTodo, viewTodo } from "@/api";
+
+interface TaskProps {
+  task: ITask;
+}
+
+const Task: React.FC<TaskProps> = ({ task }) => {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+  const [openModalView, setOpenModalView] = useState<boolean>(false);
+
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    });
+    setOpenModalEdit(false);
+    router.refresh();
+  };
+
+  const handleDeleteTask = async (id: string) => {
+    await deleteTodo(id);
+    setOpenModalDeleted(false);
+    router.refresh();
+  };
+
+  const handleViewTask = async (id: string) => {
+    await viewTodo(id);
+    setOpenModalView(false);
+    router.refresh();
+  };
+
+  return (
+    <tr key={task.id}>
+      <td className='w-full'>{task.text}</td>
+      <td className='flex gap-5'>
+        <FiEdit
+          onClick={() => setOpenModalEdit(true)}
+          cursor='pointer'
+          className='text-blue-500'
+          size={25}
+        />
+        <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className='font-bold text-lg'>Edit task</h3>
+            <div className='modal-action'>
+              <input
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                type='text'
+                placeholder='Type here'
+                className='input input-bordered w-full'
+              />
+              <button type='submit' className='btn'>
+                Submit
+              </button>
+            </div>
+          </form>
+        </Modal>
+        <FiTrash2
+          onClick={() => setOpenModalDeleted(true)}
+          cursor='pointer'
+          className='text-red-500'
+          size={25}
+        />
+        <Modal modalOpen={openModalDeleted} setModalOpen={setOpenModalDeleted}>
+          <h3 className='text-lg'>
+            Are you sure, you want to delete this task?
+          </h3>
+          <div className='modal-action'>
+            <button onClick={() => handleDeleteTask(task.id)} className='btn'>
+              Yes
+            </button>
+          </div>
+        </Modal>
+        <GrFormView
+          onClick={() => setOpenModalView(true)}
+          cursor='pointer'
+          className='text-green-500'
+          size={25}
+        />
+        <Modal modalOpen={openModalView} setModalOpen={setOpenModalView}>
+          <h3 className='text-lg'>
+          {task.text}
+          </h3>
+          
+        </Modal>
+      </td>
+    </tr>
+  );
+};
+
+export default Task;
